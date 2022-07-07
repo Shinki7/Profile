3527e9f56b468ac52aea435643977d278a31b613
Shinki7 <alexaten007@gmail.com>
2022-07-07 19:41:34 +0700

    Revisi

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@
             stars.style.left = value * 0.4 + "px";
             moon.style.top = value * 0.5 + "px";
             met.style.top = value * 1 + "px";
-            ab.style.opacity = value * 4 + "px";
         })
 }
 {
